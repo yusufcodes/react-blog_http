2410aac3b86795c82a8545d86eb1ef4c227b6cda
yusufcodes <yusufcodes@outlook.com>
2020-06-07 16:04:07 +0100

    Set up Axios to fetch some dummy data and have output the data so far

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,31 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        // Getting our data from API
+        // Sidenote: Can I jusrt use Fetch API here?
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+            // Updating the state once we receive the data
+            const posts = response.data.slice(0, 4);
+            this.setState({posts: response.data})
+        });
+    }
+
+    render() {
+        // Going through each post and creating a Post component
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title}/>
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
