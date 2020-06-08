9e65ada35d2b2ae6858450bc8dbe5bcec7dcb48a
yusufcodes <yusufcodes@outlook.com>
2020-06-08 06:19:25 +0100

    Learned about the delete method in Axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -21,6 +21,13 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/'+this.props.id)
+        .then(response => {
+            console.log(response);
+        })
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
@@ -28,9 +35,9 @@ class FullPost extends Component {
             post = (
                 <div className="FullPost">
                     <h1>{this.state.loadedPost.title}</h1>
-                    <p>{this.state.loadedPost.content}</p>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
