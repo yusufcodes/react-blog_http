d7c30709659c8460785a76c94676921304414ed4
yusufcodes <yusufcodes@outlook.com>
2020-06-08 06:15:33 +0100

    Learned how to Post data using Axios

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './NewPost.css';
 
@@ -7,6 +8,20 @@ class NewPost extends Component {
         title: '',
         content: '',
         author: 'Max'
+    }
+
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            content: this.state.content,
+            author: this.state.author
+        }
+
+        // Axios turns the data into JSON using Stringify
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        .then(response => {
+            console.log(response);
+        })
     }
 
     render () {
@@ -22,7 +37,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
