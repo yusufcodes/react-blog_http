214292bb90d61e127e30d259031bd88073a97f98
yusufcodes <yusufcodes@outlook.com>
2020-07-04 21:04:35 +0100

    Imported Switch component to only load one route at a time

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -23,9 +23,11 @@ class Blog extends Component {
                 </header>
                 { /* Setting up routes - exact means only / renders Home, not just as a prefix */}
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/:id" exact component={FullPost}/>
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
             </div>
         );
     }
